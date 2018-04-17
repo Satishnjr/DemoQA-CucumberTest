a2b3edbd217932f3cab6b3479ca6849615f9e242
sksatishreddy <sksatishreddy@gmail.com>
2018-04-17 22:51:28 +0200

    running cucumber tests using TestNG

--- src/test/java/helpers/TestRunner.java
+++ src/test/java/helpers/TestRunner.java
@@ -1,13 +1,11 @@
 package helpers;
 
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features", plugin = { "pretty",
 		"html:target/cucumber-html-report", "json:cucumber.json" }, tags = {}, monochrome=true, glue = {"helpers","step_definitions"})
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
